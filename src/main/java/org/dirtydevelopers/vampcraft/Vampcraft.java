9106d62ef1bfa812ea082f1c98bc6fa76eceabcc
Carlos Lucas <clucas@hotelbeds.com>
2020-04-20 21:46:27 +0200

    first commit

---
+++ src/main/java/org/dirtydevelopers/vampcraft/Vampcraft.java
@@ -0,0 +1,16 @@
+package org.dirtydevelopers.vampcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Vampcraft extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
